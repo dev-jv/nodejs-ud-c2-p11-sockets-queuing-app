65ca6861d7496eb1fd4c099ddbcbf107223a937d
dev-jv <dev.javierleon@gmail.com>
2021-02-26 15:22:57 -0500

    seven

--- sockets/controller.js
+++ sockets/controller.js
@@ -5,6 +5,7 @@ const ticketControl = new TicketControl();
 const socketController = socketClient => {
     // console.log(ticketControl);
     socketClient.emit('last-ticket', ticketControl.last);
+    socketClient.emit('current-state', ticketControl.last4);
 
     // socketClient.on('disconnect', () => {
     //     console.log('Disconnected client'.brightWhite, socketClient.id);
@@ -27,6 +28,7 @@ const socketController = socketClient => {
         }
 
         const ticket = ticketControl.attendTicket(desktop);
+        socketClient.broadcast.emit('current-state', ticketControl.last4);
 
         if(!ticket) {
             return callback({
