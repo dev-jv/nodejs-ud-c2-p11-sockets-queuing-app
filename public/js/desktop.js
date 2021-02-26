7574986629eb8c6479808179c5cf80d0fcd30f7c
dev-jv <dev.javierleon@gmail.com>
2021-02-25 22:15:24 -0500

    five

--- public/js/desktop.js
+++ public/js/desktop.js
@@ -1,2 +1,43 @@
 
-console.log('Desktop HTML');
+// HTML references
+const lblDesktop = document.querySelector('h1');
+const btnAttend = document.querySelector('button');
+const lblTicket = document.querySelector('small');
+const divAlert = document.querySelector('.alert');
+const lblPending = document.querySelector('#lblPending');
+
+const searchParams = new URLSearchParams(window.location.search);
+
+if(!searchParams.has('desktop')) {
+    window.location = 'index.html';
+    throw new Error('The desktop is obligatory');
+}
+
+const desktop = searchParams.get('desktop');
+console.log('desktop: ' + desktop);
+
+lblDesktop.innerText = desktop;
+
+const socket = io();
+
+socket.on('connect', () => { // "connect" event
+    // console.log('Connected > Server');
+    btnAttend.disabled = false;
+});
+
+socket.on('disconnect', () => { // "disconnect" event
+    // console.log('Disconnected > Server');
+    btnAttend.disabled = true;
+});
+
+socket.on('last-ticket', (ticket) => {
+    // lblNewTicket.innerText = ticket;
+    // console.log('last-ticket'+ ticket)
+});
+
+btnAttend.addEventListener('click', () => {
+    // socket.emit('next-ticket', null, ( ticket ) => {
+    //     lblNewTicket.innerText = ticket;
+    //     console.log('From the server: next-ticket', ticket)
+    // });
+});
--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,39 +1,39 @@
-
-// HTML references
-const lblOnline  = document.querySelector('#lblOnline');
-const lblOffline = document.querySelector('#lblOffline');
-const txtMessage = document.querySelector('#txtMessage');
-const btnSend  = document.querySelector('#btnSend');
-
-const socket = io();
-
-socket.on('connect', () => { // "connect" event
-    // console.log('Connected > Server');
-    lblOnline.style.display = '';
-    lblOffline.style.display = 'none';
-});
-
-socket.on('disconnect', () => { // "disconnect" event
-    // console.log('Disconnected > Server');
-    lblOnline.style.display = 'none';
-    lblOffline.style.display = '';
-});
-
-socket.on('send-msg', (payload) => {
-    console.log(payload)
-});
-
-btnSend.addEventListener('click', () => {
-    const msg = txtMessage.value;
-    // console.log(msg);
-    const payload = {
-        msg,
-        id: '123ABC',
-        date: new Date().getTime()
-    };
-
-    socket.emit('send-msg', payload, ( kallback ) => {
-        console.log('Response from server:', kallback);
-    });
-
-});
+//
+// // HTML references
+// const lblOnline  = document.querySelector('#lblOnline');
+// const lblOffline = document.querySelector('#lblOffline');
+// const txtMessage = document.querySelector('#txtMessage');
+// const btnSend  = document.querySelector('#btnSend');
+//
+// const socket = io();
+//
+// socket.on('connect', () => { // "connect" event
+//     // console.log('Connected > Server');
+//     lblOnline.style.display = '';
+//     lblOffline.style.display = 'none';
+// });
+//
+// socket.on('disconnect', () => { // "disconnect" event
+//     // console.log('Disconnected > Server');
+//     lblOnline.style.display = 'none';
+//     lblOffline.style.display = '';
+// });
+//
+// socket.on('send-msg', (payload) => {
+//     console.log(payload)
+// });
+//
+// btnSend.addEventListener('click', () => {
+//     const msg = txtMessage.value;
+//     // console.log(msg);
+//     const payload = {
+//         msg,
+//         id: '123ABC',
+//         date: new Date().getTime()
+//     };
+//
+//     socket.emit('send-msg', payload, ( kallback ) => {
+//         console.log('Response from server:', kallback);
+//     });
+//
+// });
